999331bdce84b04d466ccdd28ec81b886368f596
Fredrik Modéen <fredrik@modeen.se>
2021-11-08 09:10:35 +0100

    Delete UploadWebFiles.bat

--- UploadWebFiles.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-set WINSCP="%ProgramFiles%\WinSCP\WinSCP.com"
-
-xcopy /D /Y /R changelog.txt WebSiteFiles
-xcopy /D /Y /R readme.txt WebSiteFiles
-
-%WINSCP% /script=UploadWebFiles.txt
